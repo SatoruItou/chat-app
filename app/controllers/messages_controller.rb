d0a1091797ed5ac105394487c94fcbf20543cbe7
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-01-12 00:30:41 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
+      
   end
 
   def create
@@ -15,7 +16,7 @@ class MessagesController < ApplicationController
       render :index
      end
   end
-
+  
   private
 
   def message_params
